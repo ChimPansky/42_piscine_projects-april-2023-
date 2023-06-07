367923d140287db7f8d74e3f5eb99d240a127a5f
Thomas Kasbarian <thomas.kasbarian@gmail.com>
2023-06-07 13:17:49 +0200

    C09 ex00 and ex01

---
+++ C09/ex00/libtest.c
@@ -0,0 +1,7 @@
+#include "ft_header.h"
+
+int	main(void)
+{
+	ft_putstr("testing libft...");
+	return (0);
+}
---
+++ C09/ex01/includes/ft.h
@@ -0,0 +1,12 @@
+#ifndef FT_H
+# define FT_H
+
+#include <unistd.h>
+
+void	ft_putstr(char *str);
+void	ft_putchar(char c);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+void	ft_swap(int *a, int *b);
+
+# endif
